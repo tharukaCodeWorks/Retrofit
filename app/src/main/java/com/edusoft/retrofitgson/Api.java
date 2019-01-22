f460892d73b1532fe9bcbc72dffd41afd3657a57
Tharuka Lakshan <tharukawapnet@gmail.com>
2019-01-22 21:51:01 +0530

    Initial commit

---
+++ app/src/main/java/com/edusoft/retrofitgson/Api.java
@@ -0,0 +1,4 @@
+package com.edusoft.retrofitgson;
+
+public interface Api {
+}
---
+++ app/src/main/java/com/edusoft/retrofitgson/Post.java
@@ -0,0 +1,14 @@
+package com.edusoft.retrofitgson;
+
+public class Post {
+
+    private String title;
+    private String post;
+    private String date;
+    private int postId;
+    private String image;
+    private int categoryId;
+    private String shortDescription;
+    private String link;
+
+}
